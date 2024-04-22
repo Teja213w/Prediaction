1170004c8e0d87e4423b7e316e21febbb5dc78d5
Teja <60825100+Teja213w@users.noreply.github.com>
2024-04-22 18:09:22 +0530

    Update app.py

--- Predication/app.py
+++ Predication/app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model(r'C:\Users\Asus\Downloads\Stock_Market_Prediction_ML\Stock Predictions Model.keras')
+model = load_model(r'C:\Users\TEJA\Desktop')
 
 st.header('Stock Market Predictor')
 
